aed1ff043e43220c59b1b2d92fab216600980b8b
BevilKunG <devilbevilgamer@gmail.com>
2021-02-04 12:51:50 +0700

    set prettier bracket spacing config to false

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import { AppProps } from 'next/app'
-import { FC } from 'react'
+import {AppProps} from 'next/app'
+import {FC} from 'react'
 import 'tailwindcss/tailwind.css'
 
-const App: FC<AppProps> = ({ Component, pageProps }) => {
+const App: FC<AppProps> = ({Component, pageProps}) => {
   return <Component {...pageProps} />
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from 'next'
+import {NextPage} from 'next'
 
 const Home: NextPage = () => {
   return <h1>Hello World!</h1>
