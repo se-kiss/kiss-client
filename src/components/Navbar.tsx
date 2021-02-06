1f4bef622f0d1a22a83e017d73735cdc8692e4ba
BevilKunG <devilbevilgamer@gmail.com>
2021-02-06 16:13:18 +0700

    implement layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react'
+
+const Navbar: FC = () => {
+  return (
+    <div className="w-full h-14 bg-gray-200 px-8 flex flex-row  justify-between items-center">
+      <h1 className="text-xl">KiSS</h1>
+    </div>
+  )
+}
+
+export default Navbar
