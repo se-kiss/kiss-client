63dd9c60476c0b9fe58e382410352c123eab3373
BevilKunG <devilbevilgamer@gmail.com>
2021-02-07 15:06:22 +0700

    add default font in global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import {AppProps} from 'next/app'
 import {FC} from 'react'
+import {createGlobalStyle} from 'styled-components'
 import 'tailwindcss/tailwind.css'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: "Helvetica Neue", Helvetica, san-serif;
+  }
+`
+
 const App: FC<AppProps> = ({Component, pageProps}) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
