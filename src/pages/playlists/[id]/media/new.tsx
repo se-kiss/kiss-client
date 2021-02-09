62524ec00d1f111f0714e8ee3411c2e9a1c098a3
BevilKunG <devilbevilgamer@gmail.com>
2021-02-09 22:00:02 +0700

    implement video uploading button

--- src/pages/playlists/[id]/media/new.tsx
+++ src/pages/playlists/[id]/media/new.tsx
@@ -1,7 +1,7 @@
 import {NextPage} from 'next'
-import {FC, useContext} from 'react'
+import {FC, useContext, useRef} from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faNewspaper, faVideo} from '@fortawesome/free-solid-svg-icons'
+import {faNewspaper, faVideo, faUpload} from '@fortawesome/free-solid-svg-icons'
 import {Layout} from '../../../../components'
 import {
   MediaFormProvider,
@@ -33,6 +33,21 @@ const MediaType = styled.div<MediaTypeProps>`
   }
 `
 
+const HorizontalLine = styled.hr`
+  width: 100%;
+  border-color: #626aaa;
+  border-width: 1px;
+`
+
+const Button = styled.button`
+  background: #267dcd;
+  color: white;
+
+  &:focus {
+    outline: none;
+  }
+`
+
 const SideBox: FC = () => {
   const {state, dispatch} = useContext(MediaFormContext)
 
@@ -50,12 +65,13 @@ const SideBox: FC = () => {
   ]
 
   return (
-    <div className="px-4 pt-4">
+    <div className="px-4 py-4">
       <h1 className="text-lg font-bold">Select Media Type</h1>
+
       <div className="mt-4">
         {mediaTypes.map(({name, icon, type}) => (
           <MediaType
-            key={name}
+            key={type}
             className="flex flex-row items-center rounded my-2 py-2 pl-2 cursor-pointer"
             active={type === state.mediaType}
             onClick={() => dispatch({...initialMediaState, mediaType: type})}
@@ -65,19 +81,73 @@ const SideBox: FC = () => {
           </MediaType>
         ))}
       </div>
+
+      <HorizontalLine />
+
+      <div className="mt-4">
+        <Button className="w-full rounded text-xl font-medium my-1 py-1">
+          Create
+        </Button>
+      </div>
     </div>
   )
 }
 
 const ArticleForm: FC = () => {
   return (
-    <div className="w-full h-full">
-      <div className="pt-10">
+    <div className="w-full h-full px-10 py-10">
+      <div>
         <h2 className="text-md font-semibold mb-1">Title</h2>
         <input
           className="w-full text-xl rounded-sm shadow-lg py-2 pl-4"
           placeholder="Title"
         />
+      </div>
+    </div>
+  )
+}
+
+const VideoForm: FC = () => {
+  const videoInput = useRef(null)
+
+  const onUploadClick = () => {
+    videoInput.current.click()
+  }
+
+  return (
+    <div className="w-full h-full">
+      <div className="bg-black w-full h-80 flex justify-center items-center rounded-t">
+        <Button
+          className="rounded text-lg font-medium px-4 py-1"
+          onClick={onUploadClick}
+        >
+          <FontAwesomeIcon icon={faUpload} className="mr-2" />
+          <span>Upload</span>
+        </Button>
+        <input
+          type="file"
+          accept="video/*"
+          ref={videoInput}
+          className="hidden"
+        />
+      </div>
+
+      <div className="w-full h-full px-10 py-4">
+        <div>
+          <h2 className="text-md font-semibold mb-1">Title</h2>
+          <input
+            className="w-full text-xl rounded-sm shadow-lg py-2 pl-4"
+            placeholder="Title"
+          />
+        </div>
+
+        <div className="mt-8">
+          <h2 className="text-md font-semibold mb-1">Description</h2>
+          <input
+            className="w-full text-xl rounded-sm shadow-lg py-2 pl-4"
+            placeholder="Description"
+          />
+        </div>
       </div>
     </div>
   )
@@ -90,7 +160,7 @@ const Form: FC = () => {
     case MediaTypes.Article:
       return <ArticleForm />
     case MediaTypes.Video:
-      return null
+      return <VideoForm />
     default:
       return null
   }
@@ -100,7 +170,7 @@ const MediaForm: NextPage = () => {
   return (
     <MediaFormProvider>
       <Layout SideComponent={SideBox}>
-        <div className="bg-white w-10/12 h-full mx-auto mt-2 px-10 rounded shadow">
+        <div className="bg-white w-10/12 h-full mx-auto mt-2 rounded shadow">
           <Form />
         </div>
       </Layout>
