2b14725666cb36192944e13cc3f470a0630c4869
BevilKunG <devilbevilgamer@gmail.com>
2021-02-10 08:06:01 +0700

    add navbar end already-login elements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import {FC} from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faPlus, faBell} from '@fortawesome/free-solid-svg-icons'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -12,7 +14,17 @@ const BrandText = styled.h1`
 
 const LinkText = styled.h1`
   color: #626aaa;
+`
+
+const IconButton = styled.div`
+  color: #626aaa;
+  background: white;
   cursor: pointer;
+
+  &:hover {
+    color: white;
+    background: #626aaa;
+  }
 `
 
 const Button = styled.button`
@@ -25,12 +37,33 @@ const Button = styled.button`
 `
 
 const NavbarEnd: FC = () => {
+  const isLogin = true
+
+  if (!isLogin) {
+    return (
+      <div className="flex flex-row items-center">
+        <LinkText className="text-lg font-medium mx-4 cursor-pointer">Login</LinkText>
+        <Button className="px-4 py-2 text-md font-medium rounded mx-4">
+          Register
+        </Button>
+      </div>
+    )
+  }
+
   return (
     <div className="flex flex-row items-center">
-      <LinkText className="text-lg font-medium mx-4">Login</LinkText>
-      <Button className="px-4 py-2 text-md font-medium rounded mx-4">
-        Register
-      </Button>
+      <div className="flex flex-row items-center mx-4 cursor-pointer">
+        <div className="w-8 h-8 bg-white rounded-full mr-2" />
+        <LinkText className="text-lg font-medium">User</LinkText>
+      </div>
+
+      <IconButton className="w-10 h-10 shadow rounded-full mx-4 flex justify-center items-center cursor-pointer">
+        <FontAwesomeIcon icon={faPlus} className="text-lg font-medium" />
+      </IconButton>
+
+      <IconButton className="w-10 h-10 shadow rounded-full mx-4 flex justify-center items-center cursor-pointer">
+        <FontAwesomeIcon icon={faBell} className="text-lg font-medium" />
+      </IconButton>
     </div>
   )
 }
@@ -39,7 +72,9 @@ const Navbar: FC = () => {
   return (
     <Container className="fixed w-full h-14 bg-gray-200 px-12 flex flex-row  justify-between items-center">
       <Link href="/">
-        <BrandText className="text-2xl font-bold cursor-pointer">KiSS</BrandText>
+        <BrandText className="text-2xl font-bold cursor-pointer">
+          KiSS
+        </BrandText>
       </Link>
 
       <NavbarEnd />
