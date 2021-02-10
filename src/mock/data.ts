e6428bbb404190886edb4e41d898aad0483e89da
BevilKunG <devilbevilgamer@gmail.com>
2021-02-10 22:09:48 +0700

    implement playlist page

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -1,10 +1,9 @@
 export type PlaylistType = {
-  id: string,
-  name: string,
-  ownerId: string,
+  id: string
+  name: string
+  ownerId: string
   description?: string
   tagIds: string[]
-  mediaIds: string[]
 }
 
 export const playlists: PlaylistType[] = [
@@ -12,9 +11,17 @@ export const playlists: PlaylistType[] = [
     id: '1',
     name: 'Playlist 1',
     ownerId: '1',
-    description: '',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     tagIds: ['1', '2'],
-    mediaIds: ['1', '2'],
+  },
+  {
+    id: '2',
+    name: 'Playlist 2',
+    ownerId: '2',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    tagIds: ['3'],
   },
 ]
 
@@ -49,23 +56,33 @@ export enum MediaTypes {
 
 export type MediaType = {
   id: string
+  playlistId: string
   name: string
   type: MediaTypes
   content?: string
-  description?: string 
+  description?: string
 }
 
 export const media: MediaType[] = [
   {
     id: '1',
+    playlistId: '1',
     name: 'Media 1',
     type: MediaTypes.Article,
     content: '',
   },
   {
     id: '2',
+    playlistId: '1',
     name: 'Media 2',
     type: MediaTypes.Video,
+    description: '',
+  },
+  {
+    id: '3',
+    playlistId: '2',
+    name: 'Media 3',
+    type: MediaTypes.Article,
     description: '',
   },
 ]
@@ -77,7 +94,15 @@ export type UserType = {
 
 export const users: UserType[] = [
   {
+    id: '0',
+    name: 'Me',
+  },
+  {
     id: '1',
-    name: 'Author 1'
-  }
+    name: 'Author 1',
+  },
+  {
+    id: '2',
+    name: 'Author 2',
+  },
 ]
--- src/pages/demo/playlists/index.tsx
+++ src/pages/demo/playlists/index.tsx
@@ -1,15 +1,78 @@
 import {NextPage} from 'next'
-import {useContext} from 'react'
+import {useRouter} from 'next/router'
+import {FC, useContext} from 'react'
 import {Layout} from '../../../components'
+import {PlaylistType} from '../../../mock/data'
 import {MockContext} from '../../../mock/MockContext'
+import styled from 'styled-components'
+
+type TagProps = {
+  color?: string
+}
+
+const Tag = styled.div<TagProps>`
+  background: ${({color}) => (color ? color : 'white')};
+`
+
+const Card = styled.div`
+  color: #626aaa;
+`
+
+type PlaylistCardProps = {
+  playlist: PlaylistType
+}
+
+const PlaylistCard: FC<PlaylistCardProps> = ({playlist}) => {
+  const router = useRouter()
+  const {state} = useContext(MockContext)
+  const {users, tags: mockTags} = state
+
+  const {id, name, ownerId, tagIds} = playlist
+  const owner = users.find((user) => user.id === ownerId)
+  const tags = tagIds.map((tagId) =>
+    mockTags.find((mockTag) => mockTag.id === tagId)
+  )
+
+  const onCardClick = () => {
+    router.push(`/demo/playlists/${id}/media`)
+  }
+
+  return (
+    <Card className="bg-white w-9/12 rounded-lg shadow-xl p-4 my-8 cursor-pointer" onClick={onCardClick}>
+      <div className="flex flex-row items-center">
+        <div className="w-8 h-8 rounded-full bg-gray-500 mr-4" />
+
+        <div>
+          <h2 className="text-md font-medium">{owner.name}</h2>
+          <h4 className="text-sm font-normal">Feb 2</h4>
+        </div>
+      </div>
+
+      <div className="px-4 mt-2">
+        <h1 className="text-xl font-semibold">{name}</h1>
+
+        <div className="flex flex-row flex-wrap items-center mt-4">
+          {tags.map(({id, name, color}) => (
+            <Tag key={id} color={color} className="rounded mr-2 px-2 text-black">
+              {name}
+            </Tag>
+          ))}
+        </div>
+      </div>
+    </Card>
+  )
+}
 
 const Playlists: NextPage = () => {
   const {state} = useContext(MockContext)
+  const {playlists} = state
   console.log(state)
   return (
     <Layout>
-      <div>
-        <h1>Playlists</h1>
+      <div className="px-10 mt-8 mx-auto">
+        {playlists.map((playlist) => (
+          <PlaylistCard key={playlist.id} playlist={playlist} />
+        ))}
       </div>
     </Layout>
   )
