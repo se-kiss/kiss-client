ef0fbad1330bf1310201a21b82b7d393950ee822
BevilKunG <devilbevilgamer@gmail.com>
2021-02-27 16:59:20 +0700

    set up apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import {FC} from 'react'
 import {MockProvider} from '../mock/MockContext'
 import {createGlobalStyle} from 'styled-components'
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
 import 'tailwindcss/tailwind.css'
 
 const GlobalStyle = createGlobalStyle`
@@ -12,6 +13,11 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_API_URL,
+  cache: new InMemoryCache(),
+})
+
 const App: FC<AppProps> = ({Component, pageProps}) => {
   return (
     <>
@@ -19,9 +25,11 @@ const App: FC<AppProps> = ({Component, pageProps}) => {
         <title>KiSS</title>
       </Head>
       <GlobalStyle />
-      <MockProvider>
-        <Component {...pageProps} />
-      </MockProvider>
+      <ApolloProvider client={client}>
+        <MockProvider>
+          <Component {...pageProps} />
+        </MockProvider>
+      </ApolloProvider>
     </>
   )
 }
