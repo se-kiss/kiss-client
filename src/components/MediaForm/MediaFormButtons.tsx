e30debfdb1019b1f51909009525d78321ba79f50
BevilKunG <devilbevilgamer@gmail.com>
2021-02-28 11:50:17 +0700

    split outlined button component

--- src/components/MediaForm/MediaFormButtons.tsx
+++ src/components/MediaForm/MediaFormButtons.tsx
@@ -1,15 +1,5 @@
 import {FC} from 'react'
-import styled from 'styled-components'
-
-const OutlinedButton = styled.button`
-  border: 1px solid #ff8a83;
-  color: #ff8a83;
-
-  &:hover {
-    color: white;
-    background: #ff8a83;
-  }
-`
+import {OutlinedButton} from '../'
 
 const MediaFormButtons: FC = () => {
   return (
