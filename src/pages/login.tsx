0f94b9b2d76540787960b0ce1073983cbcff5ea3
wisaitas <jiwwisaitad01@hotmail.com>
2021-03-16 17:44:18 +0700

    login update validate complete

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
 import {Layout} from '../components'
 import {NextPage} from 'next'
 import styled from 'styled-components'
+import useLogin,{LoginProvider} from '../lib/useLogin'
 
 const Button = styled.button`
     border: 1px solid #ff8a83;
@@ -15,16 +16,34 @@ const Button = styled.button`
     }
 `
 
-const Login: NextPage = () => {
+const Form = () => {
+    const {state,dispatch} = useLogin()
+
+    function validateuname(name){
+        if(name === ''){return false} return true
+    }
+
+    function validatepassword(pw){
+        if(pw === ''){return false} return true
+    }
+
+    const onSubmit = () => {
+        const x = validateuname(state.userName)
+        const y = validatepassword(state.password)    
+        console.log("uname = ",x)
+        console.log("password = ",y)
+    }
     return(
-        <Layout>
-            <div className="w-full h-screen flex flex-col justify-center items-center">
+        <div className="w-full h-screen flex flex-col justify-center items-center">
                 <div className="bg-white w-8/12 h-auto px-10 pt-6 pb-20 rounded-xl shadow-xl px-14">
+                <h4 className="text-2xl text-gray-700 font-semibold text-center my-7">Login</h4>
                     <div>
                         <h3 className="text-lg text-gray-700 font-medium mb-2">Username</h3>
                         <input
                             type="text"
                             name="name"
+                            value={state.userName}
+                            onChange= { (e) => {dispatch({userName:e.target.value})}}
                             className="w-full border border-gray-300 pl-2 py-1 text-lg rounded focus:outline-none"
                         />
                     </div>
@@ -33,14 +52,25 @@ const Login: NextPage = () => {
                         <input
                             type="password"
                             name="password"
+                            value={state.password}
+                            onChange={ (e) => {dispatch({password:e.target.value})}}
                             className="w-full border border-gray-300 pl-2 py-1 text-lg rounded focus:outline-none"
                         />
                     </div>
                     <div className="mt-4">
-                        <Button>Login</Button>
+                        <Button onClick={onSubmit}>Login</Button>
                     </div>
                 </div>
             </div>
+    )
+}
+
+const Login: NextPage = () => {
+    return(
+        <Layout>
+            <LoginProvider>
+                <Form/>
+            </LoginProvider>
         </Layout>
     )
 }
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,6 +1,7 @@
 import {Layout} from '../components'
 import {NextPage} from 'next'
 import styled from 'styled-components'
+import useRegister,{RegisterProvider} from '../lib/useRegister'
 
 const Button = styled.button`
     border: 1px solid #ff8a83;  
@@ -15,10 +16,33 @@ const Button = styled.button`
     }
 `
 
-const Register: NextPage = () => {
+const Form = () => {
+    const {state,dispatch} = useRegister()
+
+    function validatename(name){
+        var regName = /^[a-zA-Z]+$/;
+        if(!regName.test(name)){return false} return true
+    }
+    
+    function validateusername(uname){
+        var reguName = /^[a-zA-Z0-9]+$/
+        if(!reguName.test(uname)){return false} return true
+    }
+
+    function confirmpassword(p,cp){
+        if(p !== cp){return false} return true
+    }
+
+    const onSubmit = () => {
+        validatename(state.firstName)
+        validatename(state.lastName)
+        validateusername(state.userName)
+        validateusername(state.password)
+        confirmpassword(state.password,state.confirmPassword)
+    
+    }
     return(
-        <Layout>
-            <div className="w-full h-screen flex flex-col justify-center items-center">
+        <div className="w-full h-screen flex flex-col justify-center items-center">
                 <div className="bg-white w-8/12 h-auto px-10 pt-6 pb-20 rounded-xl shadow-xl px-14">
                     <h4 className="text-2xl text-gray-700 font-semibold text-center my-7">Register</h4>
                     <div className="flex flex-row">
@@ -27,6 +51,8 @@ const Register: NextPage = () => {
                             <input
                                 type="text"
                                 name="firstname"
+                                value={state.firstName}
+                                onChange= { (e) => {dispatch({firstName:e.target.value})}}
                                 className="w-full border border-gray-300 pl-2 py-1 text-lg rounded focus:outline-none"
                             />
                         </div>
@@ -35,6 +61,8 @@ const Register: NextPage = () => {
                             <input
                                 type="text"
                                 name="lastname"
+                                value={state.lastName}
+                                onChange= { (e) => {dispatch({lastName:e.target.value})}}
                                 className="w-full border border-gray-300 pl-2 py-1 text-lg rounded focus:outline-none"
                             />
                         </div>
@@ -43,7 +71,9 @@ const Register: NextPage = () => {
                         <h3 className="text-lg text-gray-700 font-medium mb-2">Username</h3>
                         <input
                             type="text"
-                            name="name"
+                            name="username"
+                            value={state.userName}
+                            onChange= { (e) => {dispatch({userName:e.target.value})}}
                             className="w-full border border-gray-300 pl-2 py-1 text-lg rounded focus:outline-none"
                         />
                     </div>
@@ -52,6 +82,8 @@ const Register: NextPage = () => {
                         <input
                             type="password"
                             name="password"
+                            value={state.password}
+                            onChange= { (e) => {dispatch({password:e.target.value})}}
                             className="w-full border border-gray-300 pl-2 py-1 text-lg rounded focus:outline-none"
                         />
                     </div>
@@ -60,14 +92,25 @@ const Register: NextPage = () => {
                         <input
                             type="password"
                             name="confirmpassword"
+                            value={state.confirmPassword}
+                            onChange= { (e) => {dispatch({confirmPassword:e.target.value})}}
                             className="w-full border border-gray-300 pl-2 py-1 text-lg rounded focus:outline-none"
                         />
                     </div>
                     <div className="mt-7">
-                        <Button>Summit</Button>
+                        <Button onClick={onSubmit}>Submit</Button>
                     </div>
                 </div>
             </div>
+    )
+}
+
+const Register: NextPage = () => {
+    return(
+        <Layout>
+            <RegisterProvider>
+                <Form/>
+            </RegisterProvider>
         </Layout>
     )
 }
